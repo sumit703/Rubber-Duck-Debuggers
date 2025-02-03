c827ba070401a486d4d061a414d2b29d852166f1
sumit703 <avirajadi37@gmail.com>
2025-02-03 20:40:48 +0530

    First Commit

---
+++ assignment.py
@@ -0,0 +1,69 @@
+# a=45 
+# print("The type of a is", type(a))
+
+# b=4.5 
+# print("The type of b is", type(b))
+
+# c=2+5j
+# print("The type of c is", type(c))
+
+# d="Sumit"
+# print("The type of d is", type(d))
+
+# e=True
+# print("The type of e is", type(e))
+
+# f=(1,2,3,4)
+# print ("The type of f is", type(f))
+
+# g={'Jan':1,'Feb':2,'Mar':3}
+# print("The type of g is", type(g))
+
+# # h=[1,2,3,4]
+# print("The type of h is", type(h))
+
+'''A=45
+A_str=str(A)
+print("Integer:", A)
+print(type(A))
+print("Converted to String:",A_str)
+print(type(A_str))'''
+
+'''B="18"
+B_int=int(B)
+print("String:", B)
+print(type(B))
+print("Converted to Integer:",B_int)
+print(type(B_int))'''
+
+'''C=2.8
+C_int=int(C)
+print("Float:", C)
+print(type(C))
+print("Converted to Integer:",C_int)
+print(type(C_int))'''
+
+'''D=(1,2,3,4)
+D_list=list(D)
+print("Tuple:", D)
+print(type(D))
+print("Converted to List:",D_list)
+print(type(D_list))'''
+
+'''E="True"
+E_bool=bool(E)
+print("String:", E)
+print(type(E))
+print("Converted to Boolean:",E_bool)
+print(type(E_bool))'''
+
+
+
+string1 = "Hello"
+string2 = "World"
+
+concatenated_string = string1 + " " + string2
+print("Concatenated String:", concatenated_string)
+
+substring = concatenated_string[0:5]  
+print("Substring:", substring)
